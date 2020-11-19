a18acbd0a4f2437118d31139d60a838afbbdb6f0
Rui Castro <rui_castro@live.com.pt>
2020-11-19 00:13:42 -0100

    Added laravel-7-web-template to project

---
+++ laravel-7-web-template/resources/views/aluno.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+    <div>
+        <header></header>
+    </div>
+
+    <div>
+        <nav>
+            <ul>
+                <li>Menu1</li>
+                <li>Menu2</li>
+                <li>Menu3</li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="content">
+        <h1>FORM</h1>
+        <form action="">
+            <label for="name">Nome</label>
+            <br>
+            <input type="text" id="name" placeholder="Insira o seu nome crl!">
+            <br>
+            <label for="idade">Idade</label>
+            <br>
+            <input type="text" id="idade" placeholder="Insira a sua idade">
+            <br>
+            <button type="submit">Adicionar</button>
+        </form>
+    </div>
+
+    <div class="footer-style">
+        <footer></footer>
+    </div>
+    </body>
+</html>
