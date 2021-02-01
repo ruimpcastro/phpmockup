30627d5f4f8e308111533b75f3e8a34556c13c56
Rui Castro <rui_castro@live.com.pt>
2021-01-31 23:52:37 -0100

    Updated Estagiario.php

--- app/Estagiario.php
+++ app/Estagiario.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estagiario extends Utilizador
 {
-    //
+
+    public function projeto()
+    {
+        return $this->hasOne(Projeto::class);
+    }
+
+    public function orientador()
+    {
+        return $this->hasOne(Orientador::class);
+    }
+
+    public function entidade()
+    {
+        return $this->hasOne(Entidade::class);
+    }
+
+
 }
